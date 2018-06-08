0bc0d34f53c068bc9361c3db062745792351ef5f
Aske Mottelson <aske@mottelson.dk>
2018-06-08 14:38:19 +0300

    name

--- server/ml/flask-app.py
+++ server/ml/flask-app.py
@@ -40,5 +40,5 @@ def give_greeting():
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
